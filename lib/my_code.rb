45ba72d27444cbe6968c88cf67d783d5b6f22a25
Amy Franz <amy.franz@gmail.com>
2020-01-10 13:36:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,4 +53,16 @@ def reduce_to_all_true(random_array)
   end
   return result
 end
+
+def reduce_to_any_true(random_array)
+  i = 0 
+  result = false 
+  while i < random_array.length do 
+    if random_array[i] == true 
+      result = true
+    end
+    i += 1 
+  end
+  return result
+end
 end
